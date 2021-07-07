404afcf1b196285f9c72cb75f7061f96734be9f6
KevinFromConn <kfc21194@gmail.com>
2021-07-06 20:39:06 -0400

    flehsed out README.md and added to script.js

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# project-one
+# New Music Reccommendations (Group Project One)
+
+The goal of this project is to create a functioning application using at least two server-side APIs. Our group chose to create an application which will recommend to the user new music based on their search of genre. The APIs used are the Spotify API and (if it wants to play nice) the Apple Music API. Users will be able to search for a genre (shown on screen as "Music that fits your style") and be shown the top three results for new releases in that genre. In a future update, we plan on also showing the user the top three music-related news stories upon loading the page.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,9 @@
-var endpoint = 'https://api.spotify.com/v1/recommendations';
-var artists = '6sFIWsNpZYqfjUpaCgueju';
-var danceability = encodeURIComponent('0.9');
-
 var getAppleMusic = function() {
-    var apiUrl = 'https://api.music.apple.com/v1/catalog/{storefront}/genres'
+    var apiUrl = 'https://api.music.apple.com/v1/catalog/us/genres&q=' + userInput + '&appid='
 
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
-            response.json().then(function(data) {
+            response.json().then(function() {
                 displayGenres(genres);
             });
         } else {
@@ -19,19 +15,19 @@ var getAppleMusic = function() {
     });
 };
 
-var getSpotify = function() {
-    var apiUrl = '"https://api.spotify.com/v1/recommendations'
+const endpoint = "https://api.spotify.com/v1/recommendations";
+const artists = '6sFIWsNpZYqfjUpaCgueju';
+const danceability = encodeURIComponent('0.9');
 
-    fetch(apiUrl).then(function(response) {
-        if (response.ok) {
-            response.json().then(function(data) {
-                displayGenres(genres);
-            });
-        } else {
-            return
-        }
-    })
-    .catch(function(error) {
-        return
-    });
-};
+fetch(`${endpoint}?seed_artists=${artists}&target_danceability=${danceability}`, {
+  method: "GET",
+  headers: {
+      Authorization: `Bearer ${userAccessToken}`
+  }
+})
+.then(response => response.json())
+.then(({tracks}) => {
+  tracks.forEach(item => {
+    console.log(`${item.name} by ${item.artists[0].name}`);
+  })
+})
