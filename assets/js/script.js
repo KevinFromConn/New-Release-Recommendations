2255d9e24f1f54337e0aa603f90518d3880d964b
KevinFromConn <kfc21194@gmail.com>
2021-07-10 13:11:56 -0400

    working with functions and API in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,50 @@
 var btnEl = $("search-btn");
-var userInput = $(".search-content");
-var songTitle = $("#song-title")
-var searchHistory = json.parse(localStorage.getItem("userInput")) || [];
+var userInput = $("#search-content");
+var songTitle = $("#song-title");
+var searchItem = $("#search-item");
+var searchHistory = JSON.parse(localStorage.getItem("userInput")) || [];
 
-var searchFunction = function() {
-    var searchTerm = userInput.value;
-        if (searchTerm) {
-            getGenres(searchTerm);
-            searchHistory[searchTerm] = null;
-            localStorage.setItem("userInput", json.stringify(searchHistory))
-        }
-}
+var searchFunction = function (searchTerm) {
+  getGenres(searchTerm);
+  //   searchHistory[searchTerm] = null;
+  if (!searchHistory.includes(searchTerm)) searchHistory.push(searchTerm);
+  localStorage.setItem("userInput", JSON.stringify(searchHistory));
+};
 
-var getGenres = function(searchTerm) {
-    fetch('https://api.spotify.com/v1/recommendations/available-genre-seeds?query=' + searchTerm + '&type-genre', {
-    method: 'GET',
-    headers: 'Authorization: Bearer BQAWH96c-fu1BQBt2HcvZxF4iIxcSKJ1VvTOBlvOvZosStIIMOPumpWbhCJW-1u07CEPG3grALlJm5j3A1tXr1glzF6csJgVadhlYAlRrwvZT_nNgpSswENWvoFmyx2Hw2wNr4gGxV484A'
-}).then(function(response) {
-    response.json().then(function(data) {
-        console.log(data)
-    });
-});
-}
-var displayGenres = function(spotifyData) {
-    songTitle.innerHtml = "";
-    for (let i = 0; i < spotifyData.length; i++) {
-        songTitle.innerHtml = "";
-        
+var getGenres = function (searchTerm) {
+  fetch(
+    "https://api.spotify.com/v1/browse/new-releases?query=" +
+      searchTerm +
+      "&type=genre",
+    {
+      method: "GET",
+      headers:{
+        "Authorization": "Bearer BQARGdUEzGHnZOIrVTdv9E834mzIPnud6A0JGmgG3xxG_DUDPWOgTRoMJIEUCLxP-y11m3_oneihrkdMYuL1f9FqTCD9kZhtrWZpXLGUcZuGXvq_E8JFNYypBlbjkGhBxCk3KBWawKVi7g"
+      }
     }
-} 
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    console.log(elems);
-    var instances = M.Sidenav.init(elems, options);
-    console.log(instances);
+  ).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+    });
   });
+};
 
-var searchBtn = document.querySelector("#search-btn");
-var searchBar = document.querySelector("#search-bar");
+var displayGenres = function (spotifyData) {
+  songTitle.innerHtml = "";
+  for (let i = 0; i < spotifyData.length; i++) {
+    songTitle.innerHtml = "";
+  }
+};
 
-searchBtn.addEventListener("click", function() {
-    
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".sidenav");
+//   console.log(elems);
+  var instances = M.Sidenav.init(elems, options);
+  console.log(instances);
+});
 
-})
+function submitForm(event) {
+  event.preventDefault();
+  console.log(userInput.val());
+  searchFunction(userInput.val());
+}
