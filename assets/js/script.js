62c28f00801a00be87fca6fafd02dada665372a0
XFegan <xavierslf11@gmail.com>
2021-07-10 13:12:07 -0400

    index.html updated and nav bar responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+var options = ["New Releases", "Music News", "My Account"];
+
+console.log(M.Sidenav);
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+
+
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     console.log(elems)
